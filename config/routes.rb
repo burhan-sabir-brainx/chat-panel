e04cd4170ba0d4bb6010e512f7eb42559fd48bd0
Burhan <burhan.sabir@brainxtech.com>
2023-07-19 19:04:36 +0500

    Devise setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  # root to: 'home#index'
+  
+  devise_scope :user do
+    get "users", to: "devise/sessions#new"
+  end
+  
   
 end
